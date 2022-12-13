8a16eaa6df557d4980e745ade6b8a58d764c4063
DimaTerminator2009 <mihail.novikov0101@gmail.com>
2022-12-13 14:30:59 +0300

    first.com

---
+++ WpfApp1/Elevator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    public class Elevator
+    {
+
+        public enum ElevatorState { Move, Stop}
+        public ElevatorState state = ElevatorState.Stop;
+        int Level;
+        public int CurrentLevel = 0;
+        DispatcherTimer timer;
+        Label lbLevel;
+        public Elevator()
+        {
+           
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+           
+        }
+
+        public void MoveTo(int level, int currentLevel, Label label)
+        {
+            Level = level; // 3
+            CurrentLevel = currentLevel; // 0
+            lbLevel = label;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (CurrentLevel < Level)
+            {
+     
+          
+                CurrentLevel++;
+                lbLevel.Content = CurrentLevel;
+            }
+            else if (CurrentLevel > Level)
+            {
+        
+                CurrentLevel--;
+                lbLevel.Content = CurrentLevel;
+            }
+            else
+            state = ElevatorState.Stop;
+        }
+    
+    }
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+  
+    public partial class MainWindow : Window
+    {
+        int level = 0;
+        int currentLevel = 0;
+        Elevator elevator = new Elevator();
+      
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            elevator.state = Elevator.ElevatorState.Move;
+
+            level = Convert.ToInt32(tbLevel.Text);
+            elevator.MoveTo(level, currentLevel, lbCurrentLevel);
+            currentLevel = level;
+            //lbCurrentLevel.Content = elevator.CurrentLevel;
+          
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (elevator.state != Elevator.ElevatorState.Move)
+            {
+                btnStart.IsEnabled = false;
+                recDoors.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (elevator.state != Elevator.ElevatorState.Move)
+            {
+                btnStart.IsEnabled = true;
+                recDoors.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
